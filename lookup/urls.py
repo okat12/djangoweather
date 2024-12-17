169416971433ed11f090ef5ec89961403a0593f7
Walter Emovon <wemovon43@gmail.com>
2024-12-17 13:43:07 -0600

    added comment to views.py

--- lookup/urls.py
+++ lookup/urls.py
@@ -1,8 +1,12 @@
+#this is my views.py file
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('about.html', views.about, name="about"),
-]
+
+
+ ]
  
